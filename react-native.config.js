c522b346e1661d7b01c5901e6a261eb0af4ef4b4
richardbendler <richard.bendler@gmail.com>
2026-01-11 15:45:35 +0100

    Fix autolinking paths for native modules

--- react-native.config.js
+++ react-native.config.js
@@ -1,3 +1,23 @@
+const path = require("path");
+
 module.exports = {
-  dependencies: {},
+  dependencies: {
+    "@react-native-async-storage/async-storage": {
+      platforms: {
+        android: {
+          sourceDir: path.resolve(
+            __dirname,
+            "node_modules/@react-native-async-storage/async-storage/android"
+          ),
+        },
+      },
+    },
+    "@react-native-voice/voice": {
+      platforms: {
+        android: {
+          sourceDir: path.resolve(__dirname, "node_modules/@react-native-voice/voice/android"),
+        },
+      },
+    },
+  },
 };
